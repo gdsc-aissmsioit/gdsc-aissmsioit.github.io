d51920ab1439358cb1892672f4ef90c6fb20a900
SatvikNalavade <nalavadesatvik@gmail.com>
2023-08-20 10:21:54 +0530

    New Event Added

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -1,5 +1,20 @@
 const events = [
   {
+    id: 17,
+    name: "Info Session",
+    date: "2023-08-21",
+    dateAlt: "21st August 2023 🗓️",
+    day: "Monday",
+    about:
+      "Join us on August 21st, from 12 pm onwards, as we embark on a thrilling journey into the world of technology at the GDSC (Google Developer Student Clubs) Info Session and Inauguration. This event is not just an introduction; it's a celebration of innovation and collaboration. Be inspired by our special guest, an ex-GDSC Lead, who will share their insights, guiding us to new horizons of tech exploration. Don't miss out – connect, learn, and be a part of this exciting chapter in our GDSC community.",
+    speaker: "Core Team",
+    guest: "Prarthana Chandak",
+    eventLink:
+      "https://gdsc.community.dev/events/details/developer-student-clubs-aissms-institute-of-information-technology-pune-presents-info-session/",
+    imageSrc: "https://ibb.co/3B6Pntf/Info-Session.jpg",
+    dataAos: "fade-up",
+  },
+  {
     id: 16,
     name: "Google Summer of Code with GDSC! ☀️",
     date: "2022-04-03T03:00:00Z",
