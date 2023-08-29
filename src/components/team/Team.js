255cd96478b88138d58946ccfd85a974364e6130
SatvikNalavade <nalavadesatvik@gmail.com>
2023-08-29 13:30:21 +0530

    added helmet to team page

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 import "./Team.css";
 
-import Team_Img from "../../images/team/team2.jpg";
+import Team_Svg from "../../images/team/Team.gif";
 
 import Slider from "react-slick";
 
@@ -21,23 +21,12 @@ import Design from "./design";
 
 import TeamCard from "./TeamCard";
 
+import { Helmet } from "react-helmet";
+
 const Team = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    const imgFluids = document.getElementsByClassName("img-fluid");
-    Array.from(imgFluids).forEach((img) => {
-      img.addEventListener("mouseover", () => {
-        img.classList.remove("out");
-        img.classList.add("over");
-      });
-
-      img.addEventListener("mouseout", () => {
-        img.classList.remove("over");
-        img.classList.add("out");
-      });
-    });
-
     if (id) {
       document.getElementById(id).scrollIntoView({
         behavior: "smooth",
@@ -126,6 +115,19 @@ const Team = () => {
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Team - GDSC</title>
+        <meta name="title" content="Team - GDSC" />
+        <meta
+          name="description"
+          content="Have a look at the great minds of GDSC AISSMS IOIT. None of us, including me, ever do great things. But we can all do small things, with great love, and together we can do something wonderful."
+        />
+        <meta
+          name="keywords"
+          content="gdsc team, gdsc aissms team, gdsc aissms ioit team, dsc, dsc aissms team, dsc aissms ioit team, gdsc ioit team, dsc ioit team, ioit dsc team, aissms dsc team, aissms ioit dsc team, aissms ioit gdsc team, gdsc-aissms-ioit team, gdsc-ioit team, dsc-aissms-ioit team, dsc-ioit team"
+        />
+      </Helmet>
       <div className="container-fluid team-heading">
         <div className="row">
           <div
@@ -139,7 +141,7 @@ const Team = () => {
           >
             <img
               className="cursor-pointer Team_Image"
-              src={Team_Img}
+              src={Team_Svg}
               alt="Team_Image"
             />
           </div>
@@ -152,54 +154,45 @@ const Team = () => {
             data-aos-anchor=".example-selector"
             data-aos-anchor-placement="top-center"
           >
-            <h1 className="font-weight-bold">Meet Our Team</h1>
-            <p className="font-weight-light">
-              Have a look at the great minds of GDSC AISSMS IOIT. <br />
-              "None of us, including me, ever do great things. But we can all do
-              small things, with great love, and together we can do something
-              wonderful."
+            <h1 className="font-weight-bold">Meet The Core Team</h1>
+            <p className="font-weight-light core_team_text">
+              Connect with the great minds who have been working rigorously to
+              create a positive impact on the community! "It is literally true
+              that you can succeed best and quickest by helping others to
+              succeed." – Napolean Hill
             </p>
           </div>
         </div>
       </div>
 
-      <div className="container team-members">
+      <div className="dots">
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+
+      <div className="container team-members teams">
         <Fade up>
           <div id="gdsc_lead" className="row">
-            <div className="col-lg-6 col-sm-6 member-body">
-              <Fade up>
-                <h3 className="font-weight-bold">Faculty Advisor</h3>
-              </Fade>
-              <img
-                src="https://i.ibb.co/16LcLWP/Wagh-Sir.png"
-                className="img-fluid lead-img"
-                alt="Faculty_Advisor"
-              />
-              <h5>Dr. K. S. Wagh Sir</h5>
-              <span></span>
-              <div className="social-ico">
-                <a target="_blank" rel="noreferrer" href="#0">
-                  <i className="fa fa-linkedin ico-link"></i>
-                </a>
-                <a target="_blank" rel="noreferrer" href="#0">
-                  <i className="fa fa-github ico-github"></i>
-                </a>
-                <a target="_blank" rel="noreferrer" href="#0">
-                  <i className="fa fa-instagram ico-insta"></i>
-                </a>
-              </div>
-            </div>
-
-            <div className="col-lg-6 col-sm-6 member-body">
+            <div className="col-lg-6 col-sm-6">
               <Fade up>
                 <h3 className="font-weight-bold">GDSC Lead</h3>
               </Fade>
-              <img
-                src="https://i.ibb.co/cCyVvHd/sid.png"
-                className="img-fluid lead-img"
-                alt="Lead_Image"
-              />
-              <h5>Siddhant Sangale</h5>
+
+              <div>
+                <span
+                  className="Tooltip gdsc_lead_tooltip"
+                  data-tooltip="Hi! I'm Prarthana. I love to sleep but coding is my coffee ;) I enjoy problem solving and competitive coding. I'm currently learning Node.js and I'm loving it. Can't wait to meet you guys in a GDSC offline event!"
+                >
+                  <img
+                    src="https://i.ibb.co/cCyVvHd/sid.png"
+                    className="img-fluid lead-img"
+                    alt="Lead_Image"
+                  />
+                </span>
+              </div>
+
+              <h5 className="mt-4">Siddhant Sangale</h5>
+
               <span></span>
               <div className="social-ico">
                 <a
@@ -229,6 +222,29 @@ const Team = () => {
                   rel="noreferrer"
                 >
                   <i className="fa fa-link ico-link"></i>
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-6 col-sm-6 teams">
+              <Fade up>
+                <h3 className="font-weight-bold">Faculty Advisor</h3>
+              </Fade>
+              <img
+                src="https://i.ibb.co/16LcLWP/Wagh-Sir.png"
+                className="img-fluid lead-img"
+                alt="Faculty_Advisor"
+              />
+              <h5 className="mt-4">Dr. K. S. Wagh Sir</h5>
+              <span></span>
+              <div className="social-ico">
+                <a target="_blank" rel="noreferrer" href="#0">
+                  <i className="fa fa-linkedin ico-link"></i>
+                </a>
+                <a target="_blank" rel="noreferrer" href="#0">
+                  <i className="fa fa-github ico-github"></i>
+                </a>
+                <a target="_blank" rel="noreferrer" href="#0">
+                  <i className="fa fa-instagram ico-insta"></i>
                 </a>
               </div>
             </div>
